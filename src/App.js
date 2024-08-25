6322e48dcd5a18437533d708d28dba6e75d554fb
ajaytidke28297 <ajaytidke28297@gmail.com>
2024-08-25 19:21:58 +0530

    add useReducer and various screens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
-import React from "react";
-import DateCounter from "./DateCounter";
+import React, { useEffect, useReducer } from "react";
+import Error from "./Error";
+import Header from "./Header";
+import Loader from "./Loader";
+import Main from "./Main";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Unkown action");
+  }
+}
 
 function App() {
-  return <DateCounter />;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions?.length;
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((e) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
+    </div>
+  );
 }
 
 export default App;
